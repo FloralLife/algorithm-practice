ea96a73441b230c37a8f73b02f5cd3ea963ed5a5
FloralLife <ocard9611@naver.com>
2022-02-05 02:20:23 +0900

    프로그래머스 N으로 표현

--- algorithm-practice/N으로 표현.js
+++ algorithm-practice/N으로 표현.js
@@ -1,22 +1,40 @@
-function matchNumber(n, number){
-	return n === number;
+function matchNumber(n, number) {
+  return n === number;
 }
 
 function solution(N, number) {
-	const expressedNumbers = Array.from(Array(8), () => []);
-	for(const (e, idx) of expressedNumbers) {
-			let n = '';
-			for(let i = 0; i <= idx; i++){
-					n += N;
-			}
-			n = parseInt(n);
-			if (matchNumber(n, number)) return idx + 1;
-			e.push(n);
-	}
+  const expressedNumbers = Array.from(Array(8), () => []);
+  for (const [idx, e] of expressedNumbers.entries()) {
+    let n = "";
+    for (let i = 0; i <= idx; i++) {
+      n += N;
+    }
+    n = parseInt(n);
+    if (matchNumber(n, number)) return idx + 1;
+    e.push(n);
+  }
 
-	for (let i = 0; i < 8; i++){
-			for(let j = 0; j < i; j++){
-
-			}
-	}
+  for (let i = 0; i < 8; i++) {
+    for (let j = 0; j < i; j++) {
+      for (const n1 of expressedNumbers[j]) {
+        for (const n2 of expressedNumbers[i - j - 1]) {
+          let result = n1 + n2;
+          if (matchNumber(result, number)) return i + 1;
+          expressedNumbers[i].push(result);
+          result = n1 - n2;
+          if (matchNumber(result, number)) return i + 1;
+          expressedNumbers[i].push(result);
+          result = n1 * n2;
+          if (matchNumber(result, number)) return i + 1;
+          expressedNumbers[i].push(result);
+          if (n2 !== 0) {
+            result = Math.floor(n1 / n2);
+            if (matchNumber(result, number)) return i + 1;
+            expressedNumbers[i].push(result);
+          }
+        }
+      }
+    }
+  }
+  return -1;
 }
